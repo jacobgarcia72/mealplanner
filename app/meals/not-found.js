c33fac76f976c67241eea8d41d7625af9efc267c
Jacob Garcia <yockub@gmail.com>
2026-04-04 19:36:36 -0400

    meal fetching

---
+++ app/meals/not-found.js
@@ -0,0 +1,6 @@
+export default function NotFound() {
+    return <main className="not-found">
+        <h1>Meal Not Found</h1>
+        <p>The meal you are looking for could not be found.</p>
+    </main>
+}
