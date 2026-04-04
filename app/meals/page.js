177ebf6cc92c18e71b9717fc18482c3543c7c023
Jacob Garcia <yockub@gmail.com>
2026-04-04 19:17:08 -0400

    loading and error states

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,10 +2,14 @@ import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />
+}
 
 export default function MealsPage() {
-
-  const meals = getMeals();
 
   return (
     <>
@@ -21,7 +25,9 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
